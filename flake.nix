01857ca53a39d9291800406e1b27692fcc9b450a
Borno09890 <tashinirtijab@gmail.com>
2026-06-05 00:26:45 +0600

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -15,44 +16,55 @@
     home-manager,
     lazyvim,
     ...
-  }: {
+  }: let
+    system = "x8664-linux";
+    pkgs = nixpkgs.legacyPackages.${system}; # ← ADD THIS
+  in {
     nixosConfigurations.borno = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      imports = [lazyvim.homeManagerModules.default];
-      programs.lazyvim = {
-        enable = true;
-        extras = {
-          lang.nix.enable = true;
-          lang.lua.enable = true;
-          lang.json.enable = true;
-        };
-        extraPackages = with pkgs; [
-          css-checker
-          css-html-js-minify
-          prettierd
-          html-xml-utils
-          nixd
-          alejandra
-          lua-language-server
-          stylua
-        ];
-        treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
-          css
-          html
-          lua
-          nix
-          javascript
-          typescript
-        ];
-      };
+      inherit system;
+      specialArgs = {inherit inputs pkgs;};
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.borno = import ./home.nix;
+          home-manager.users.borno = {
+            config,
+            pkgs,
+            ...
+          }: {
+            imports = [
+              inputs.lazyvim.homeManagerModules.default # ← Move import here
+              ./home.nix
+            ];
+            programs.lazyvim = {
+              enable = true;
+              extras = {
+                lang.nix.enable = true;
+                lang.lua.enable = true;
+                lang.json.enable = true;
+              };
+              extraPackages = with pkgs; [
+                css-checker
+                css-html-js-minify
+                prettierd
+                html-xml-utils
+                nixd
+                alejandra
+                lua-language-server
+                stylua
+              ];
+              treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+                css
+                html
+                lua
+                nix
+                javascript
+                typescript
+              ];
+            };
+          };
         }
       ];
     };
