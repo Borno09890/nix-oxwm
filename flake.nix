4aa8725651916c10483239701cf5e8a6a51e3638
Borno09890 <tashinirtijab@gmail.com>
2026-06-05 00:41:43 +0600

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    inputs.nixpkgs.follows = "nixpkgs";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      lazyvim.url = "github:pfassina/lazyvim-nix";
+    };
+    lazyvim = {
+      url = "github:pfassina/lazyvim-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = inputs @ {
